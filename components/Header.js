e4ebc4e3f87e12e327ddc51c44c62aad581ab088
Dave <87390532+vadedes@users.noreply.github.com>
2022-05-28 22:10:07 +0800

    Added register and login pages

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import styles from '@/styles/Header.module.css';
 import Link from 'next/link';
 import Search from './Search';
@@ -23,6 +24,13 @@ export default function Header() {
                     <li>
                         <Link href='/events/add'>
                             <a>Add Event</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/account/login'>
+                            <a className='btn-secondary btn-icon'>
+                                <FaSignInAlt /> Login
+                            </a>
                         </Link>
                     </li>
                 </ul>
